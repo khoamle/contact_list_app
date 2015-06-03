2ac1b715accd50afe0a180d96e0e0e00f09118cf
Khoa Le <khoaminhle5@gmail.com>
2015-06-03 17:55:32 -0500

    initial repo

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -22,7 +22,7 @@ class ContactsController < ApplicationController
   def update
     @contact = Contact.find_by(id: params[:id])
     @contact.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
-    redirect_to "/"
+    redirect_to "/contacts/#{@contact.id}"
   end
 
   def destroy
