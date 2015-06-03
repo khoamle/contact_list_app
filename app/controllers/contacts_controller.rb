f3803dc012b421dfbec6308714dc3428f267dd90
Khoa Le <khoaminhle5@gmail.com>
2015-06-02 19:18:35 -0500

    add REST convention

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,32 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def new
+    
+  end
+
+  def show
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def edit
+    @contact = Contact.find_by(id: params[:id])
+  end
+
+  def create 
+    Contact.create(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+  end
+
+  def update
+    @contact = Contact.find_by(id: params[:id])
+    @contact.update(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], phone_number: params[:phone_number])
+    redirect_to "/"
+  end
+
+  def destroy
+    @contact = Contact.find_by(id: params[:id])
+    @contact.destroy
+  end
+end
