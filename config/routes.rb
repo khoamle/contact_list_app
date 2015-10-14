61dae6bb4c3fd4db7bfd3d584735beb5307f9ea6
Khoa Le <khoaminhle5@gmail.com>
2015-10-14 10:41:12 -0500

    fix home button issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  root to: 'contacts#index'
   resources :contacts
+  root :to => 'contacts#index'
 
   namespace :api do 
     namespace :v1 do
